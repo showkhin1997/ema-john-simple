472fe50d32b5d9a4b1efd6d85bf2ea0ea511a5eb
showkhin1997 <showkhin15@gmail.com>
2021-09-23 00:20:19 +0600

    button icon add

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
 const Product = (props) => {
     // console.log(props);
     const { name, price, seller, stock, img } = props.product;
+    const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className="product">
             <div>
@@ -14,7 +17,9 @@ const Product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>Price: ${price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
-                <button onClick={() => props.handaleAddToCart(props.product)} className="btn-regular">add to cart</button>
+                <button
+                    onClick={() => props.handaleAddToCart(props.product)}
+                    className="btn-regular">{cartIcon}add to cart</button>
             </div>
         </div>
     );
