5c82ea5c6b11bbebf737f92ab35ff77648ecdf57
showkhin1997 <showkhin15@gmail.com>
2021-09-23 00:32:01 +0600

    added more cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,12 +8,19 @@ const Cart = (props) => {
         total = total + product.price;
     }
 
+    const shipping = 15;
+    const tax = (total + shipping) * 10;
+    const grandTotal = total + shipping + tax;
+
     return (
         <div>
             <h3>Order Summary</h3>
             <h5>Items ordered: {props.cart.length}</h5>
             <br />
-            <p>Price: {total}</p>
+            <p>Price: {total.toFixed(2)}</p>
+            <p>Shipping: {shipping}</p>
+            <p>Tax: {tax.toFixed(2)}</p>
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
