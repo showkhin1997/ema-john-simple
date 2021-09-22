0ebd89da228d19e8d22a06388f0a19717240d280
showkhin1997 <showkhin15@gmail.com>
2021-09-23 00:06:05 +0600

    button handale add

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    console.log(props.product);
+    // console.log(props);
     const { name, price, seller, stock, img } = props.product;
     return (
         <div className="product">
@@ -14,7 +14,7 @@ const Product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>Price: ${price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
-                <button className="btn-regular">add to cart</button>
+                <button onClick={() => props.handaleAddToCart(props.product)} className="btn-regular">add to cart</button>
             </div>
         </div>
     );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
     useEffect(() => {
         fetch('./products.JSON')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
 
+    const handaleAddToCart = product => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className="shop-container">
             <div className="product-container">
@@ -17,12 +24,12 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.key}
                         product={product}
+                        handaleAddToCart={handaleAddToCart}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
-                <h3>Order Summary</h3>
-                <h5>Items ordered:</h5>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
